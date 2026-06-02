d510d650354eb2c522facac97e3554aca071e89e
Dani <danielcerezorodas@gmail.com>
2026-06-01 23:45:19 -0400

    force dynamic agente

--- app/agente/page.js
+++ app/agente/page.js
@@ -1,5 +1,7 @@
 "use client";
 
+export const dynamic = 'force-dynamic';
+
 import { useState } from "react";
 import AgentLogin from "../components/AgentLogin";
 import AgentPanel from "../components/AgentPanel";
